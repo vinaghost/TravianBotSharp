3a60d8d7b8274958f58ef378c716f15b76bedddb
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-11-24 21:37:05 +0700

    add disable contextual help command (#26)

---
+++ MainCore/Commands/Step/DisableContextualHelp/IDisableOptionCommand.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+using MainCore.Entities;
+
+namespace MainCore.Commands.Step.DisableContextualHelp
+{
+    public interface IDisableOptionCommand
+    {
+        Task<Result> Execute(AccountId accountId);
+    }
+}
