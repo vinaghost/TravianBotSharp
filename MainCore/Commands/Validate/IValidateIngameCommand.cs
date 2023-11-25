e508b24b2ba1f32d7985b6182489deea90d412e4
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-11-25 11:31:20 +0700

    validate login & ingame command

---
+++ MainCore/Commands/Validate/IValidateIngameCommand.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+using MainCore.Entities;
+
+namespace MainCore.Commands.Validate
+{
+    public interface IValidateIngameCommand
+    {
+        bool Value { get; }
+
+        Task<Result> Execute(AccountId accountId);
+    }
+}
---
+++ MainCore/Commands/Validate/ValidateIngameCommand.cs
@@ -0,0 +1,35 @@
+﻿using FluentResults;
+using MainCore.Entities;
+using MainCore.Infrasturecture.AutoRegisterDi;
+using MainCore.Parsers;
+using MainCore.Services;
+
+namespace MainCore.Commands.Validate
+{
+    [RegisterAsTransient]
+    public class ValidateIngameCommand : IValidateIngameCommand
+    {
+        private readonly IChromeManager _chromeManager;
+        private readonly IUnitOfParser _unitOfParser;
+
+        public ValidateIngameCommand(IChromeManager chromeManager, IUnitOfParser unitOfParser)
+        {
+            _chromeManager = chromeManager;
+            _unitOfParser = unitOfParser;
+        }
+
+        public bool Value { get; private set; }
+
+        public async Task<Result> Execute(AccountId accountId)
+        {
+            await Task.CompletedTask;
+            var chromeBrowser = _chromeManager.Get(accountId);
+            var html = chromeBrowser.Html;
+
+            var fieldButton = _unitOfParser.NavigationBarParser.GetResourceButton(html);
+
+            Value = fieldButton is not null;
+            return Result.Ok();
+        }
+    }
+}
