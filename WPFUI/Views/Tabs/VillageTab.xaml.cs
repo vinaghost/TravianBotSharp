6db029237babb8badd2a53ca0110c223ddbf79f1
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-11-28 14:13:52 +0700

    fix

--- WPFUI/Views/Tabs/VillageTab.xaml.cs
+++ WPFUI/Views/Tabs/VillageTab.xaml.cs
@@ -28,20 +28,20 @@ namespace WPFUI.Views.Tabs
                 // tabs
                 this.OneWayBind(ViewModel, vm => vm.VillageTabStore.NoVillageViewModel, v => v.NoVillage.ViewModel).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.VillageTabStore.BuildViewModel, v => v.Build.ViewModel).DisposeWith(d);
-                this.OneWayBind(ViewModel, vm => vm.VillageTabStore.InfoViewModel, v => v.Info.ViewModel).DisposeWith(d);
+                //this.OneWayBind(ViewModel, vm => vm.VillageTabStore.InfoViewModel, v => v.Info.ViewModel).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.VillageTabStore.VillageSettingViewModel, v => v.Settings.ViewModel).DisposeWith(d);
 
                 // visible
                 this.OneWayBind(ViewModel, vm => vm.VillageTabStore.IsNoVillageTabVisible, v => v.NoVillageTab.Visibility).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.VillageTabStore.IsNormalTabVisible, v => v.BuildTab.Visibility).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.VillageTabStore.IsNormalTabVisible, v => v.SettingsTab.Visibility).DisposeWith(d);
-                this.OneWayBind(ViewModel, vm => vm.VillageTabStore.IsNormalTabVisible, v => v.InfoTab.Visibility).DisposeWith(d);
+                //this.OneWayBind(ViewModel, vm => vm.VillageTabStore.IsNormalTabVisible, v => v.InfoTab.Visibility).DisposeWith(d);
 
                 // selected
                 this.Bind(ViewModel, vm => vm.VillageTabStore.NoVillageViewModel.IsActive, v => v.NoVillageTab.IsSelected).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.VillageTabStore.BuildViewModel.IsActive, v => v.BuildTab.IsSelected).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.VillageTabStore.VillageSettingViewModel.IsActive, v => v.SettingsTab.IsSelected).DisposeWith(d);
-                this.Bind(ViewModel, vm => vm.VillageTabStore.InfoViewModel.IsActive, v => v.InfoTab.IsSelected).DisposeWith(d);
+                //this.Bind(ViewModel, vm => vm.VillageTabStore.InfoViewModel.IsActive, v => v.InfoTab.IsSelected).DisposeWith(d);
             });
         }
     }
