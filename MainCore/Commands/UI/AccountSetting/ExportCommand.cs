96ec62036097141a159b2710f510c2fb774ca1f1
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-11-29 22:26:35 +0700

    account setting view model

---
+++ MainCore/Commands/UI/AccountSetting/ExportCommand.cs
@@ -0,0 +1,40 @@
+﻿using MainCore.Entities;
+using MainCore.Repositories;
+using MainCore.Services;
+using MediatR;
+using System.Text.Json;
+
+namespace MainCore.Commands.UI.AccountSetting
+{
+    public class ExportCommand : IRequest
+    {
+        public AccountId AccountId { get; }
+
+        public ExportCommand(AccountId accountId)
+        {
+            AccountId = accountId;
+        }
+    }
+
+    public class ExportCommandHandler : IRequestHandler<ExportCommand>
+    {
+        private readonly IUnitOfRepository _unitOfRepository;
+        private readonly IDialogService _dialogService;
+
+        public ExportCommandHandler(IUnitOfRepository unitOfRepository, IDialogService dialogService)
+        {
+            _unitOfRepository = unitOfRepository;
+            _dialogService = dialogService;
+        }
+
+        public async Task Handle(ExportCommand request, CancellationToken cancellationToken)
+        {
+            var accountId = request.AccountId;
+            var path = _dialogService.SaveFileDialog();
+            var settings = _unitOfRepository.AccountSettingRepository.Get(accountId);
+            var jsonString = JsonSerializer.Serialize(settings);
+            await File.WriteAllTextAsync(path, jsonString, cancellationToken);
+            _dialogService.ShowMessageBox("Information", "Settings exported");
+        }
+    }
+}
--- MainCore/UI/ViewModels/Abstract/TabViewModelBase.cs
+++ MainCore/UI/ViewModels/Abstract/TabViewModelBase.cs
@@ -1,37 +1,37 @@
 ﻿using ReactiveUI;
 using System.Reactive;
+using System.Reactive.Linq;
 
 namespace MainCore.UI.ViewModels.Abstract
 {
     public abstract class TabViewModelBase : ViewModelBase
     {
         private bool _isActive;
-        private readonly ReactiveCommand<bool, Unit> TabStatusChanged;
+        private readonly ReactiveCommand<Unit, Unit> Active;
+        private readonly ReactiveCommand<Unit, Unit> Deactive;
 
         public TabViewModelBase()
         {
-            TabStatusChanged = ReactiveCommand.CreateFromTask<bool>(TabStatusChangedHandler);
+            Active = ReactiveCommand.CreateFromTask(OnActive);
+            Deactive = ReactiveCommand.CreateFromTask(OnDeactive);
 
-            this.WhenAnyValue(x => x.IsActive)
-                .InvokeCommand(TabStatusChanged);
+            var isActiveObservable = this.WhenAnyValue(x => x.IsActive);
+
+            isActiveObservable
+                .Where(x => x == true)
+                .Select(x => Unit.Default)
+                .InvokeCommand(Active);
+
+            isActiveObservable
+                .Where(x => x == false)
+                .Select(x => Unit.Default)
+                .InvokeCommand(Deactive);
         }
 
         public bool IsActive
         {
             get => _isActive;
             set => this.RaiseAndSetIfChanged(ref _isActive, value);
-        }
-
-        private async Task TabStatusChangedHandler(bool isActive)
-        {
-            if (isActive)
-            {
-                await OnActive();
-            }
-            else
-            {
-                await OnDeactive();
-            }
         }
 
         protected virtual Task OnActive()
