d213d3c0b98442283ab82ff47930ace4502c17a3
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-12-13 00:50:40 +0700

    add modify imported job

--- MainCore/Commands/UI/Build/ImportCommand.cs
+++ MainCore/Commands/UI/Build/ImportCommand.cs
@@ -24,6 +24,7 @@ namespace MainCore.Commands.UI.Build
         private readonly UnitOfRepository _unitOfRepository;
         private readonly IDialogService _dialogService;
         private readonly IMediator _mediator;
+        private readonly List<int> _excludedLocations = new() { 26, 39, 40 }; //main building, rallypoint and wall
 
         public ImportCommandHandler(UnitOfRepository unitOfRepository, IDialogService dialogService, IMediator mediator)
         {
@@ -50,7 +51,7 @@ namespace MainCore.Commands.UI.Build
             var accountId = request.AccountId;
             var villageId = request.VillageId;
 
-            var modifiedJobs = GetModifiedJobs(jobs);
+            var modifiedJobs = GetModifiedJobs(villageId, jobs);
             _unitOfRepository.JobRepository.AddRange(villageId, modifiedJobs);
 
             await _mediator.Publish(new JobUpdated(accountId, villageId), cancellationToken);
@@ -58,17 +59,24 @@ namespace MainCore.Commands.UI.Build
             _dialogService.ShowMessageBox("Information", "Jobs imported");
         }
 
-        private IEnumerable<JobDto> GetModifiedJobs(List<JobDto> jobs)
+        private IEnumerable<JobDto> GetModifiedJobs(VillageId villageId, List<JobDto> jobs)
         {
+            var buildings = _unitOfRepository.BuildingRepository.GetBuildingItems(villageId);
+
+            var changedLocations = new Dictionary<int, int>();
             foreach (var job in jobs)
             {
                 switch (job.Type)
                 {
                     case JobTypeEnums.NormalBuild:
                         {
-                            var content = JsonSerializer.Deserialize<NormalBuildPlan>(job.Content);
-                            if (IsResourceField(content)) continue;
-                            yield return job;
+                            var plan = JsonSerializer.Deserialize<NormalBuildPlan>(job.Content);
+                            if (plan.Type.IsResourceField()) continue;
+
+                            Modify(buildings, plan, changedLocations);
+                            job.Content = GetContent(plan);
+
+                            if (IsValidPlan(buildings, plan)) yield return job;
                             continue;
                         }
                     case JobTypeEnums.ResourceBuild:
@@ -82,9 +90,80 @@ namespace MainCore.Commands.UI.Build
             }
         }
 
-        private static bool IsResourceField(NormalBuildPlan plan)
+        private static string GetContent(NormalBuildPlan plan)
         {
-            return plan.Type.IsResourceField();
+            return JsonSerializer.Serialize(plan);
+        }
+
+        private static bool IsValidPlan(List<BuildingItem> buildings, NormalBuildPlan plan)
+        {
+            var building = buildings.FirstOrDefault(x => x.Location == plan.Location);
+            if (building is null) return false;
+            if (building.JobLevel >= plan.Level) return false;
+
+            if (building.Type == BuildingEnums.Site) building.Type = plan.Type;
+            building.JobLevel = plan.Level;
+            return true;
+        }
+
+        private void Modify(List<BuildingItem> buildings, NormalBuildPlan plan, Dictionary<int, int> changedLocations)
+        {
+            if (plan.Type.IsMultipleBuilding())
+            {
+                if (ModifyMultiple(buildings, plan)) return;
+            }
+            else
+            {
+                if (ModifySame(buildings, plan)) return;
+            }
+
+            if (_excludedLocations.Contains(plan.Location)) return;
+
+            ModifyRandom(buildings, plan, changedLocations);
+        }
+
+        private static bool ModifyMultiple(List<BuildingItem> buildings, NormalBuildPlan plan)
+        {
+            var largestLevelBuilding = buildings.Where(x => x.Type == plan.Type).OrderByDescending(x => x.Level).FirstOrDefault();
+            if (largestLevelBuilding is not null && largestLevelBuilding.Type.GetMaxLevel() < largestLevelBuilding.Level)
+            {
+                plan.Location = largestLevelBuilding.Location;
+                return true;
+            }
+            return false;
+        }
+
+        private static bool ModifySame(List<BuildingItem> buildings, NormalBuildPlan plan)
+        {
+            var sameTypeBuilding = buildings.Where(x => x.Type == plan.Type).FirstOrDefault();
+            if (sameTypeBuilding is not null)
+            {
+                if (sameTypeBuilding.Location != plan.Location)
+                {
+                    sameTypeBuilding.Location = plan.Location;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private void ModifyRandom(List<BuildingItem> buildings, NormalBuildPlan plan, Dictionary<int, int> changedLocations)
+        {
+            var freeLocations = buildings
+               .Where(x => x.Type == BuildingEnums.Site)
+               .Select(x => x.Location)
+               .Where(x => !_excludedLocations.Contains(x))
+               .ToList();
+
+            var randomLocation = GetRandomLocation(freeLocations);
+            changedLocations[plan.Location] = randomLocation;
+
+            plan.Location = randomLocation;
+        }
+
+        private static int GetRandomLocation(List<int> freeLocations)
+        {
+            return freeLocations[Random.Shared.Next(0, freeLocations.Count - 1)];
         }
     }
 }
