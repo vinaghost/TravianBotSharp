23845bfef549ed982a840ccd44f5aaefc758076d
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-03-16 19:53:34 +0700

    job train troop task

---
+++ MainCore/Tasks/JobTrainTroopTask.cs
@@ -0,0 +1,29 @@
+﻿using FluentResults;
+using MainCore.Commands;
+using MainCore.Infrasturecture.AutoRegisterDi;
+using MainCore.Repositories;
+using MainCore.Tasks.Base;
+using MediatR;
+
+namespace MainCore.Tasks
+{
+    [RegisterAsTransient(withoutInterface: true)]
+    public class JobTrainTroopTask : VillageTask
+    {
+        public JobTrainTroopTask(UnitOfCommand unitOfCommand, UnitOfRepository unitOfRepository, IMediator mediator) : base(unitOfCommand, unitOfRepository, mediator)
+        {
+        }
+
+        protected override async Task<Result> Execute()
+        {
+            await Task.CompletedTask;
+            return Result.Ok();
+        }
+
+        protected override void SetName()
+        {
+            var name = _unitOfRepository.VillageRepository.GetVillageName(VillageId);
+            _name = $"Job training troop in {name}";
+        }
+    }
+}
