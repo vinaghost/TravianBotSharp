3f7b2594451a91657e44cc33878be1d2c9ed7e39
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-04-06 17:52:10 +0700

    start adventure task

--- MainCore/Tasks/StartAdventureTask.cs
+++ MainCore/Tasks/StartAdventureTask.cs
@@ -1,11 +1,11 @@
 ﻿using FluentResults;
 using MainCore.Commands;
 using MainCore.Commands.Base;
-using MainCore.Commands.Features;
 using MainCore.Commands.Features.Step.StartAdventure;
-using MainCore.Commands.Validate;
+using MainCore.Commands.Navigate;
 using MainCore.Common.Errors;
 using MainCore.Infrasturecture.AutoRegisterDi;
+using MainCore.Parsers;
 using MainCore.Repositories;
 using MainCore.Services;
 using MainCore.Tasks.Base;
@@ -16,15 +16,19 @@ namespace MainCore.Tasks
     [RegisterAsTransient(withoutInterface: true)]
     public class StartAdventureTask : AccountTask
     {
-        private readonly ICommandHandler<ValidateAdventureCommand, bool> _validateAdventureCommand;
-        private readonly ICommandHandler<GetDurationAdventureCommand, TimeSpan> _getDurationAdventureCommand;
+        private readonly ICommandHandler<ToAdventurePageCommand> _toAdventurePageCommand;
+        private readonly ICommandHandler<ExploreAdventureCommand> _exploreAdventureCommand;
         private readonly ITaskManager _taskManager;
+        private readonly IChromeManager _chromeManager;
+        private readonly UnitOfParser _unitOfParser;
 
-        public StartAdventureTask(UnitOfCommand unitOfCommand, UnitOfRepository unitOfRepository, IMediator mediator, ICommandHandler<ValidateAdventureCommand, bool> validateAdventureCommand, ICommandHandler<GetDurationAdventureCommand, TimeSpan> getDurationAdventureCommand, ITaskManager taskManager) : base(unitOfCommand, unitOfRepository, mediator)
+        public StartAdventureTask(UnitOfCommand unitOfCommand, UnitOfRepository unitOfRepository, IMediator mediator, ITaskManager taskManager, IChromeManager chromeManager, UnitOfParser unitOfParser, ICommandHandler<ToAdventurePageCommand> toAdventurePageCommand, ICommandHandler<ExploreAdventureCommand> exploreAdventureCommand) : base(unitOfCommand, unitOfRepository, mediator)
         {
-            _validateAdventureCommand = validateAdventureCommand;
-            _getDurationAdventureCommand = getDurationAdventureCommand;
             _taskManager = taskManager;
+            _chromeManager = chromeManager;
+            _unitOfParser = unitOfParser;
+            _toAdventurePageCommand = toAdventurePageCommand;
+            _exploreAdventureCommand = exploreAdventureCommand;
         }
 
         protected override async Task<Result> Execute()
@@ -37,19 +41,23 @@ namespace MainCore.Tasks
             result = await _unitOfCommand.UpdateAccountInfoCommand.Handle(new(AccountId), CancellationToken);
             if (result.IsFailed) return result.WithError(new TraceMessage(TraceMessage.Line()));
 
-            result = await _validateAdventureCommand.Handle(new(AccountId), CancellationToken);
-            if (result.IsFailed) return result.WithError(new TraceMessage(TraceMessage.Line()));
+            var chromeBrowser = _chromeManager.Get(AccountId);
 
-            var canStartAdventure = _validateAdventureCommand.Value;
+            var html = chromeBrowser.Html;
+
+            var canStartAdventure = _unitOfParser.HeroParser.CanStartAdventure(html);
             if (!canStartAdventure) return Result.Ok();
 
-            result = await _mediator.Send(new StartAdventureCommand(AccountId), CancellationToken);
+            result = await _toAdventurePageCommand.Handle(new(AccountId), CancellationToken);
             if (result.IsFailed) return result.WithError(new TraceMessage(TraceMessage.Line()));
 
-            result = await _getDurationAdventureCommand.Handle(new(AccountId), CancellationToken);
+            result = await _exploreAdventureCommand.Handle(new(AccountId), CancellationToken);
             if (result.IsFailed) return result.WithError(new TraceMessage(TraceMessage.Line()));
 
-            await SetNextExecute(_getDurationAdventureCommand.Value);
+            html = chromeBrowser.Html;
+            var adventureDuration = _unitOfParser.HeroParser.GetAdventureDuration(html);
+
+            await SetNextExecute(adventureDuration);
 
             return Result.Ok();
         }
