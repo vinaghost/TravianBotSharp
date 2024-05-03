042ae874637708ba1d80945f5b43b750b0de7dd5
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-05-03 17:37:39 +0700

    navigation tab parser

--- MainCore/Commands/Navigate/SwitchTabCommand.cs
+++ MainCore/Commands/Navigate/SwitchTabCommand.cs
@@ -1,6 +1,4 @@
-﻿using HtmlAgilityPack;
-
-namespace MainCore.Commands.Navigate
+﻿namespace MainCore.Commands.Navigate
 {
     public class SwitchTabCommand : ICommand
     {
@@ -17,24 +15,17 @@ namespace MainCore.Commands.Navigate
     [RegisterAsTransient]
     public class SwitchTabCommandHandler : ICommandHandler<SwitchTabCommand>
     {
-        private readonly INavigationTabParser _navigationTabParser;
-
-        public SwitchTabCommandHandler(INavigationTabParser navigationTabParser)
-        {
-            _navigationTabParser = navigationTabParser;
-        }
-
         public async Task<Result> Handle(SwitchTabCommand request, CancellationToken cancellationToken)
         {
             var chromeBrowser = request.ChromeBrowser;
             var index = request.Index;
 
             var html = chromeBrowser.Html;
-            var count = _navigationTabParser.CountTab(html);
+            var count = CountTab(html);
             if (index > count) return Retry.OutOfIndexTab(index, count);
-            var tab = _navigationTabParser.GetTab(html, index);
+            var tab = GetTab(html, index);
             if (tab is null) return Retry.NotFound($"{index}", "tab");
-            if (_navigationTabParser.IsTabActive(tab)) return Result.Ok();
+            if (IsTabActive(tab)) return Result.Ok();
 
             Result result;
             result = await chromeBrowser.Click(By.XPath(tab.XPath));
@@ -44,16 +35,53 @@ namespace MainCore.Commands.Navigate
             {
                 var doc = new HtmlDocument();
                 doc.LoadHtml(driver.PageSource);
-                var count = _navigationTabParser.CountTab(doc);
+                var count = CountTab(doc);
                 if (index > count) return false;
-                var tab = _navigationTabParser.GetTab(doc, index);
+                var tab = GetTab(doc, index);
                 if (tab is null) return false;
-                if (!_navigationTabParser.IsTabActive(tab)) return false;
+                if (!IsTabActive(tab)) return false;
                 return true;
             };
             result = await chromeBrowser.Wait(tabActived, cancellationToken);
             if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
             return Result.Ok();
+        }
+
+        private static HtmlNode GetNavigationBar(HtmlDocument doc)
+        {
+            var navigationBar = doc.DocumentNode
+             .Descendants("div")
+             .FirstOrDefault(x => x.HasClass("contentNavi") && x.HasClass("subNavi"));
+            return navigationBar;
+        }
+
+        private static IEnumerable<HtmlNode> GetTabs(HtmlDocument doc)
+        {
+            var navigationBar = GetNavigationBar(doc);
+            if (navigationBar is null) return Enumerable.Empty<HtmlNode>();
+            var tabs = navigationBar
+                .Descendants("a")
+                .Where(x => x.HasClass("tabItem"));
+            return tabs;
+        }
+
+        private static int CountTab(HtmlDocument doc)
+        {
+            var count = GetTabs(doc)
+                .Count();
+            return count;
+        }
+
+        private static HtmlNode GetTab(HtmlDocument doc, int index)
+        {
+            var tab = GetTabs(doc)
+                .ElementAt(index);
+            return tab;
+        }
+
+        private static bool IsTabActive(HtmlNode node)
+        {
+            return node.HasClass("active");
         }
     }
 }
--- MainCore/Tasks/ClaimQuestTask.cs
+++ MainCore/Tasks/ClaimQuestTask.cs
@@ -6,8 +6,13 @@ namespace MainCore.Tasks
     [RegisterAsTransient(withoutInterface: true)]
     public class ClaimQuestTask : VillageTask
     {
-        public ClaimQuestTask(IChromeManager chromeManager, IMediator mediator, IVillageRepository villageRepository) : base(chromeManager, mediator, villageRepository)
+        private readonly IQuestParser _questParser;
+        private readonly DelayClickCommand _delayClickCommand;
+
+        public ClaimQuestTask(IChromeManager chromeManager, IMediator mediator, IVillageRepository villageRepository, IQuestParser questParser, DelayClickCommand delayClickCommand) : base(chromeManager, mediator, villageRepository)
         {
+            _questParser = questParser;
+            _delayClickCommand = delayClickCommand;
         }
 
         protected override async Task<Result> Execute()
@@ -18,7 +23,7 @@ namespace MainCore.Tasks
             result = await _mediator.Send(new ToQuestPageCommand(chromeBrowser), CancellationToken);
             if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
 
-            result = await _mediator.Send(new CollectRewardCommand(AccountId, chromeBrowser), CancellationToken);
+            result = await CollectReward(chromeBrowser, CancellationToken);
             if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
 
             await _mediator.Publish(new StorageUpdated(AccountId, VillageId), CancellationToken);
@@ -29,6 +34,48 @@ namespace MainCore.Tasks
         {
             var village = _villageRepository.GetVillageName(VillageId);
             _name = $"Claim quest in {village}";
+        }
+
+        private async Task<Result> CollectReward(IChromeBrowser chromeBrowser, CancellationToken cancellationToken)
+        {
+            HtmlDocument html;
+            Result result;
+            do
+            {
+                if (cancellationToken.IsCancellationRequested) return Cancel.Error;
+                html = chromeBrowser.Html;
+                var quest = _questParser.GetQuestCollectButton(html);
+
+                if (quest is null)
+                {
+                    result = await ClaimAccountQuest(chromeBrowser);
+                    if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
+                    return Result.Ok();
+                }
+
+                result = await chromeBrowser.Click(By.XPath(quest.XPath));
+                if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
+                await _delayClickCommand.Execute(AccountId);
+            }
+            while (_questParser.IsQuestClaimable(html));
+            return Result.Ok();
+        }
+
+        private async Task<Result> ClaimAccountQuest(IChromeBrowser chromeBrowser)
+        {
+            Result result;
+            result = await _mediator.Send(new SwitchTabCommand(chromeBrowser, 1));
+            if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
+            await _delayClickCommand.Execute(AccountId);
+
+            var quest = _questParser.GetQuestCollectButton(chromeBrowser.Html);
+
+            if (quest is null) return Result.Ok();
+
+            result = await chromeBrowser.Click(By.XPath(quest.XPath));
+            if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
+
+            return Result.Ok();
         }
     }
 }
