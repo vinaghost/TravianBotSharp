de05d4ca755560f77fbd25033fa8c53bfd5e59a0
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-05-03 16:54:17 +0700

    account info task

--- MainCore/Commands/Update/UpdateAccountInfoCommand.cs
+++ MainCore/Commands/Update/UpdateAccountInfoCommand.cs
@@ -12,14 +12,12 @@
         private readonly IChromeManager _chromeManager;
         private readonly IMediator _mediator;
 
-        private readonly IAccountInfoParser _accountInfoParser;
         private readonly IAccountInfoRepository _accountInfoRepository;
 
-        public UpdateAccountInfoCommandHandler(IChromeManager chromeManager, IMediator mediator, IAccountInfoParser accountInfoParser, IAccountInfoRepository accountInfoRepository)
+        public UpdateAccountInfoCommandHandler(IChromeManager chromeManager, IMediator mediator, IAccountInfoRepository accountInfoRepository)
         {
             _chromeManager = chromeManager;
             _mediator = mediator;
-            _accountInfoParser = accountInfoParser;
             _accountInfoRepository = accountInfoRepository;
         }
 
@@ -27,12 +25,48 @@
         {
             var chromeBrowser = _chromeManager.Get(command.AccountId);
             var html = chromeBrowser.Html;
-            var dto = _accountInfoParser.Get(html);
+            var dto = Get(html);
             _accountInfoRepository.Update(command.AccountId, dto);
 
             await _mediator.Publish(new AccountInfoUpdated(command.AccountId), cancellationToken);
 
             return Result.Ok();
+        }
+
+        private static AccountInfoDto Get(HtmlDocument doc)
+        {
+            var dto = new AccountInfoDto()
+            {
+                Gold = GetGold(doc),
+                Silver = GetSilver(doc),
+                HasPlusAccount = HasPlusAccount(doc),
+                Tribe = TribeEnums.Any,
+            };
+            return dto;
+        }
+
+        private static int GetGold(HtmlDocument doc)
+        {
+            var goldNode = doc.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("ajaxReplaceableGoldAmount"));
+            if (goldNode is null) return -1;
+            return goldNode.InnerText.ParseInt();
+        }
+
+        private static int GetSilver(HtmlDocument doc)
+        {
+            var silverNode = doc.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("ajaxReplaceableSilverAmount"));
+            if (silverNode is null) return -1;
+            return silverNode.InnerText.ParseInt();
+        }
+
+        private static bool HasPlusAccount(HtmlDocument doc)
+        {
+            var market = doc.DocumentNode.Descendants("a").FirstOrDefault(x => x.HasClass("market") && x.HasClass("round"));
+            if (market is null) return false;
+
+            if (market.HasClass("green")) return true;
+            if (market.HasClass("gold")) return false;
+            return false;
         }
     }
 }
