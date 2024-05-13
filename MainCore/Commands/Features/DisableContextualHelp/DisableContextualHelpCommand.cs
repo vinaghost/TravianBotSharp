70c98b41df7b6b35e8f17efa4f2762706bd6ebee
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-05-13 16:15:08 +0700

    99 item per page for report

--- MainCore/Commands/Features/DisableContextualHelp/DisableContextualHelpCommand.cs
+++ MainCore/Commands/Features/DisableContextualHelp/DisableContextualHelpCommand.cs
@@ -11,6 +11,21 @@
             result = await chromeBrowser.Click(By.XPath(option.XPath));
             if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
 
+            option = GetHiddenReportImageOption(html);
+            if (option is null) return Retry.NotFound("hide report image", "option");
+            result = await chromeBrowser.Click(By.XPath(option.XPath));
+            if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
+
+            option = GetMessagePerPageOption(html);
+            if (option is null) return Retry.NotFound("report per page", "option");
+            result = await chromeBrowser.InputTextbox(By.XPath(option.XPath), "99");
+            if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
+
+            option = GetTroopMovementsPerPageOption(html);
+            if (option is null) return Retry.NotFound("troop movements per page", "option");
+            result = await chromeBrowser.InputTextbox(By.XPath(option.XPath), "99");
+            if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
+
             var button = GetSubmitButton(html);
             result = await chromeBrowser.Click(By.XPath(button.XPath));
             if (result.IsFailed) return result.WithError(TraceMessage.Error(TraceMessage.Line()));
@@ -24,6 +39,24 @@
         private static HtmlNode GetHideContextualHelpOption(HtmlDocument doc)
         {
             var node = doc.GetElementbyId("hideContextualHelp");
+            return node;
+        }
+
+        private static HtmlNode GetMessagePerPageOption(HtmlDocument doc)
+        {
+            var node = doc.GetElementbyId("epp");
+            return node;
+        }
+
+        private static HtmlNode GetTroopMovementsPerPageOption(HtmlDocument doc)
+        {
+            var node = doc.GetElementbyId("troopMovementsPerPage");
+            return node;
+        }
+
+        private static HtmlNode GetHiddenReportImageOption(HtmlDocument doc)
+        {
+            var node = doc.GetElementbyId("flag_hiden_report_image");
             return node;
         }
 
