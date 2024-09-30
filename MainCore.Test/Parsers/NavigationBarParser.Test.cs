1e8e89921b73bfc97837de57d59e1f9ae318504b
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-09-30 17:12:44 +0700

    NavigationBarParser

---
+++ MainCore.Test/Parsers/NavigationBarParser.Test.cs
@@ -0,0 +1,17 @@
+﻿namespace MainCore.Test.Parsers
+{
+    public class NavigationBarParser : BaseParser
+    {
+        private const string Buildings = "Parsers/NavigationBar/Buildings.html";
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void GetDorfButton(int input)
+        {
+            _html.Load(Buildings);
+            var actual = MainCore.Parsers.NavigationBarParser.GetDorfButton(_html, input);
+            actual.Should().NotBeNull();
+        }
+    }
+}
