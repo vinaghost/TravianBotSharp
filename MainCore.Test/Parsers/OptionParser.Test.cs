6b5f7a01dd8af6d9570447202356e67981a21bff
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-09-30 17:27:59 +0700

    OptionParser

---
+++ MainCore.Test/Parsers/OptionParser.Test.cs
@@ -0,0 +1,43 @@
+﻿namespace MainCore.Test.Parsers
+{
+    public class OptionParser : BaseParser
+    {
+        private const string Buildings = "Parsers/Option/Buildings.html";
+        private const string OptionsPage = "Parsers/Option/OptionsPage.html";
+        private const string ResourcesWithContextHelp = "Parsers/Option/ResourcesWithContextHelp.html";
+
+        [Theory]
+        [InlineData(Buildings, false)]
+        [InlineData(ResourcesWithContextHelp, true)]
+        public void IsContextualHelpEnable(string file, bool expected)
+        {
+            _html.Load(file);
+            var actual = MainCore.Parsers.OptionParser.IsContextualHelpEnable(_html);
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GetOptionButton()
+        {
+            _html.Load(OptionsPage);
+            var result = MainCore.Parsers.OptionParser.GetOptionButton(_html);
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void GetHideContextualHelpOption()
+        {
+            _html.Load(OptionsPage);
+            var result = MainCore.Parsers.OptionParser.GetHideContextualHelpOption(_html);
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void GetSubmitButton()
+        {
+            _html.Load(OptionsPage);
+            var result = MainCore.Parsers.OptionParser.GetSubmitButton(_html);
+            result.Should().NotBeNull();
+        }
+    }
+}
