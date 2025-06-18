e9d6150554c0c811ee4142e4f34f1c4b3f262771
kikuman <kikuman@users.noreply.github.com>
2025-06-18 20:08:36 +0700

    Display real troop names in attack tab

--- WPFUI/Views/Tabs/Villages/AttackTab.xaml.cs
+++ WPFUI/Views/Tabs/Villages/AttackTab.xaml.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Collections.Generic;
+using MainCore.Enums;
+using MainCore.Services;
 using MainCore.UI.ViewModels.Tabs.Villages;
+using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
+using System.Collections.Immutable;
 using System.Reactive.Disposables;
 
 namespace WPFUI.Views.Tabs.Villages
@@ -11,6 +16,17 @@ namespace WPFUI.Views.Tabs.Villages
 
     public partial class AttackTab : AttackTabBase
     {
+        private static readonly ImmutableDictionary<TribeEnums, string[]> TroopNames = new Dictionary<TribeEnums, string[]>
+        {
+            {TribeEnums.Romans, new[] {"Legionnaire","Praetorian","Imperian","Equites Legati","Equites Imperatoris","Equites Caesaris","Battering Ram","Fire Catapult","Senator","Settler"}},
+            {TribeEnums.Teutons, new[] {"Clubswinger","Spearman","Axeman","Scout","Paladin","Teutonic Knight","Ram","Catapult","Chief","Settler"}},
+            {TribeEnums.Gauls, new[] {"Phalanx","Swordsman","Pathfinder","Theutates Thunder","Druidrider","Haeduan","Ram","Trebuchet","Chieftain","Settler"}},
+            {TribeEnums.Nature, new[] {"Rat","Spider","Snake","Bat","Wild Boar","Wolf","Bear","Crocodile","Tiger","Elephant"}},
+            {TribeEnums.Natars, new[] {"Pikeman","Thorned Warrior","Guardsman","Birds Of Prey","Axerider","Natarian Knight","War Elephant","Ballista","Natarian Emperor","Settler"}},
+            {TribeEnums.Egyptians, new[] {"Slave Militia","Ash Warden","Khopesh Warrior","Sopdu Explorer","Anhur Guard","Resheph Chariot","Ram","Stone Catapult","Nomarch","Settler"}},
+            {TribeEnums.Huns, new[] {"Mercenary","Bowman","Spotter","Steppe Rider","Marksman","Marauder","Ram","Catapult","Logades","Settler"}},
+        }.ToImmutableDictionary();
+
         public AttackTab()
         {
             InitializeComponent();
@@ -55,8 +71,29 @@ namespace WPFUI.Views.Tabs.Villages
                 T10.ViewModel = troops[9];
                 T11.ViewModel = troops[10];
 
+                this.WhenAnyValue(x => x.ViewModel.Tribe)
+                    .Subscribe(UpdateTroopNames)
+                    .DisposeWith(d);
+
                 this.BindCommand(ViewModel, vm => vm.SendCommand, v => v.SendButton).DisposeWith(d);
             });
+        }
+
+        private void UpdateTroopNames(TribeEnums tribe)
+        {
+            if (!TroopNames.TryGetValue(tribe, out var names)) return;
+
+            T1.Text = names[0];
+            T2.Text = names[1];
+            T3.Text = names[2];
+            T4.Text = names[3];
+            T5.Text = names[4];
+            T6.Text = names[5];
+            T7.Text = names[6];
+            T8.Text = names[7];
+            T9.Text = names[8];
+            T10.Text = names[9];
+            T11.Text = "Hero";
         }
     }
 }
