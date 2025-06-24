d3d4190b546ebe6c92616c7cc1f5f65828f211bd
VINAGHOST <nguyentrungnhan87@gmail.com>
2025-06-24 14:51:52 +0700

    UI tab

---
+++ MainCore/UI/ViewModels/Tabs/HeroFarmingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MainCore.UI.ViewModels.Abstract;
+
+namespace MainCore.UI.ViewModels.Tabs
+{
+    [RegisterSingleton<HeroFarmingViewModel>]
+    public class HeroFarmingViewModel : AccountTabViewModelBase
+    {
+        protected override async Task Load(AccountId accountId)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ WPFUI/Views/Tabs/HeroFarmingTab.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MainCore.UI.ViewModels.Tabs;
+using ReactiveUI;
+
+namespace WPFUI.Views.Tabs
+{
+    public class HeroFarmingTabBase : ReactiveUserControl<HeroFarmingViewModel>
+    {
+    }
+
+    /// <summary>
+    /// Interaction logic for HeroFarmingTab.xaml
+    /// </summary>
+    public partial class HeroFarmingTab : HeroFarmingTabBase
+    {
+        public HeroFarmingTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
