7166158d7cd90315dd687afb5814c18eda8e6c01
VINAGHOST <nguyentrungnhan87@gmail.com>
2025-07-07 20:54:24 +0700

    add trigger task on save account setting

--- MainCore/Commands/UI/Misc/SaveAccountSettingCommand.cs
+++ MainCore/Commands/UI/Misc/SaveAccountSettingCommand.cs
@@ -7,7 +7,8 @@
 
         private static async ValueTask HandleAsync(
             Command command,
-            AppDbContext context
+            AppDbContext context,
+            ITaskManager taskManager
             )
         {
             await Task.CompletedTask;
@@ -20,6 +21,53 @@
                     .Where(x => x.AccountId == accountId.Value)
                     .Where(x => x.Setting == setting.Key)
                     .ExecuteUpdate(x => x.SetProperty(x => x.Value, setting.Value));
+            }
+
+            if (settings.ContainsKey(AccountSettingEnums.EnableAutoLoadVillageBuilding))
+            {
+                if (settings[AccountSettingEnums.EnableAutoLoadVillageBuilding] == 1)
+                {
+                    var villagesSpec = new MissingBuildingVillagesSpec(accountId);
+                    var villages = context.Villages
+                        .WithSpecification(villagesSpec)
+                        .ToList();
+
+                    foreach (var village in villages)
+                    {
+                        var task = new UpdateBuildingTask.Task(accountId, village);
+                        if (!taskManager.IsExist<UpdateBuildingTask.Task>(accountId, village))
+                        {
+                            taskManager.Add(task);
+                        }
+                    }
+                }
+                else
+                {
+                    var villagesSpec = new VillagesSpec(accountId);
+                    var villages = context.Villages
+                        .WithSpecification(villagesSpec)
+                        .ToList();
+                    foreach (var village in villages)
+                    {
+                        taskManager.Remove<CompleteImmediatelyTask.Task>(accountId, village);
+                    }
+                }
+            }
+
+            if (settings.ContainsKey(AccountSettingEnums.EnableAutoStartAdventure))
+            {
+                if (settings[AccountSettingEnums.EnableAutoStartAdventure] == 1)
+                {
+                    var task = new StartAdventureTask.Task(accountId);
+                    if (task.CanStart(context) && !taskManager.IsExist<StartAdventureTask.Task>(accountId))
+                    {
+                        taskManager.Add(task);
+                    }
+                }
+                else
+                {
+                    taskManager.Remove<StartAdventureTask.Task>(accountId);
+                }
             }
         }
     }
